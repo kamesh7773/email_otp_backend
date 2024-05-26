e221b2ed92c64af9e6902c33716815be5fa54297
kamesh7773 <kameshsinghaaa64@gmail.com>
2024-05-26 17:01:25 +0530

    commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(cors({origin: '*'}));
 app.use(express.json());
 app.use("/api",require("./routes/app.routes"));
 
+app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(4500, function(){
     console.log("server Started");
 });
