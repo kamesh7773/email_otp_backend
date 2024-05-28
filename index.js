04e8dbd66e30c0524007f8ddd8b4dfc08237313f
kamesh7773 <kameshsinghaaa64@gmail.com>
2024-05-28 08:54:09 +0530

    Email OTP Project Completed ✅

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
-app.use(cors({origin: '*'}));
+// allow all url's
+app.use(cors({ origin: "*" }));
 
 app.use(express.json());
-app.use("/",require("./routes/app.routes"));
+app.use("/", require("./routes/app.routes"));
 
-app.get("/", (req, res) => res.send("Email OTP server running successfully ✅"));
-app.listen(4500, function(){
-    console.log("server Started successfully ✅");
+app.get("/", (req, res) =>
+  res.send("Email OTP server running successfully ✅")
+);
+app.listen(4500, function () {
+  console.log("server Started successfully ✅");
 });
--- routes/app.routes.js
+++ routes/app.routes.js
@@ -1,9 +1,9 @@
-const otpController = require('../controllers/otp.controller');
+const otpController = require("../controllers/otp.controller");
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.post('/otp-login',otpController.otpLogin);
-router.post('/otp-verify',otpController.verifyOTP);
+router.post("/otp-login", otpController.otpLogin);
+router.post("/otp-verify", otpController.verifyOTP);
 
 module.exports = router;
